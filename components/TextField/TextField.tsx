f91a22bd9be31ab01c5ff7c454fcdfc5d225dd08
joshua Irving B. Fabricante <joshua.starkiller115@gmail.com>
2021-10-18 14:26:00 +0800

    Initial Commit

---
+++ components/TextField/TextField.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Input} from "@chakra-ui/react"
+
+
+interface Props {
+    label:string;
+    value?:string;
+    handleChange:React.ChangeEventHandler<HTMLInputElement>
+}
+
+export const TextField:React.FC<Props> = ({label, value, handleChange}) => {
+
+    return (
+        <div>
+            <label><b>{label}</b></label>
+            <br/>
+            <Input type='text' variant='filled' mb={'3'} value={value} onChange={handleChange}/>
+        </div>
+    )
+}
